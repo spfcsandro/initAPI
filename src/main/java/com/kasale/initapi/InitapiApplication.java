03fe9171c16594cee9530def691438a65f1a1f96
Sandro Andrade <sandro.andrade@ofm.local>
2018-01-08 08:26:20 -0300

    Arquivos iniciais do projeto

---
+++ src/main/java/com/kasale/initapi/InitapiApplication.java
@@ -0,0 +1,12 @@
+package com.kasale.initapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitapiApplication.class, args);
+	}
+}
